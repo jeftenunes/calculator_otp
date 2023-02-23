ff760d47d3981cda654798f7ddcb3980b3723060
jeftenunes <jeftenunes@hotmail.com>
2023-02-22 20:04:20 -0500

    design template

--- lib/calculator_otp.ex
+++ lib/calculator_otp.ex
@@ -1,18 +1,25 @@
 defmodule CalculatorOtp do
-  @moduledoc """
-  Documentation for `CalculatorOtp`.
-  """
+  alias CalculatorOtp.Core, as: Core
 
-  @doc """
-  Hello world.
+  def start(initial_state) do
+    spawn(fn -> run(initial_state) end)
+  end
 
-  ## Examples
+  def run(state) do
+    state
+    |> Core.do_work()
+    |> run()
+  end
 
-      iex> CalculatorOtp.hello()
-      :world
+  def loop(state) do
+    receive do
+      :some_message ->
+        Core.do_work(state)
 
-  """
-  def hello do
-    :world
+      :another_message ->
+        Core.do_another_work(state)
+    end
+
+    loop(state)
   end
 end
---
+++ lib/calculator_otp/core/calculator_otp_core.ex
@@ -0,0 +1,11 @@
+defmodule CalculatorOtp.Core do
+  def do_work(state) do
+    IO.inspect(state)
+    state
+  end
+
+  def do_another_work(state) do
+    IO.inspect(state)
+    state
+  end
+end
