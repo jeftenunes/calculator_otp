3de51f2d030bf15bda8183f44c3625e0e61b0214
jeftenunes <jeftenunes@hotmail.com>
2023-02-22 19:40:21 -0500

    first commit

---
+++ lib/calculator_otp.ex
@@ -0,0 +1,18 @@
+defmodule CalculatorOtp do
+  @moduledoc """
+  Documentation for `CalculatorOtp`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CalculatorOtp.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/calculator_otp_test.exs
@@ -0,0 +1,8 @@
+defmodule CalculatorOtpTest do
+  use ExUnit.Case
+  doctest CalculatorOtp
+
+  test "greets the world" do
+    assert CalculatorOtp.hello() == :world
+  end
+end
